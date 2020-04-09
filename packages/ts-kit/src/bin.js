81ba58c3b2342358824164f72df7121fcb1ef77c
leecheneler <lacheneler@gmail.com>
2020-04-09 11:41:04 +0100

    updated bin launch

--- packages/ts-kit/src/bin.js
+++ packages/ts-kit/src/bin.js
@@ -5,8 +5,10 @@ const path = require("path");
 // Args to forward onto the ts-kit process
 const [, , ...args] = process.argv;
 
-spawnSync(
-  require.resolve("ts-node").replace("index", "bin"),
+const result = spawnSync(
+  require.resolve("ts-node").replace("index", "bin-script"),
   [path.resolve(__dirname, "cli.ts"), ...args],
   { stdio: "inherit" }
 );
+
+process.exit(result.status);
